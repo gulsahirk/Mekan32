6fc0fea7b20d6cc255771115137690972b467a5e
gulsahirk <gulsahirk01gmail.com>
2021-01-17 01:55:59 +0300

    sonhali

--- app.js
+++ app.js
@@ -42,7 +42,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-app.listen(3000, () => {
-	console.log("server started")
-})
+
 module.exports = app;
